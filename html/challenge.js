ab9d352089335d3f0ca1f18d935be7e2d3a1ee11
Pat Raynor <raynorpat@gmail.com>
2023-03-07 13:04:38 -0500

    Shortented the Yes and No text

--- html/challenge.js
+++ html/challenge.js
@@ -3,13 +3,13 @@ $(document).ready (function(){
         setTimeout(function () {
             window.location = 'https://go.rallyup.com/dwrotarybourbon';
         }, 2000);
-        $('.errors').text('Success! You are now being redirected to the raffle...');
+        $('.errors').text('You are now being redirected to the raffle.');
         $('.errors').addClass("green");
         $('.main_container').hide();
     });
 
     $('.no').click(function (){
-        $('.errors').text('We are sorry! You must be 21 years of age or older to enter the raffle.');
+        $('.errors').text('You must be at least 21 years of age to enter the raffle.');
         $('.errors').addClass("red");
         $('.main_container').hide();
     });
